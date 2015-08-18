6b2b6e57a00585f31a9e15abf3f73bf1c4c63082
Herculano Campos <herkulano@gmail.com>
2015-08-18 15:18:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,89 @@
+# Two Dimensional Histogram
+
+**d3.hist2d** plugin implements a non-blocking **rectangular binning**. It's useful for displaying a scatterplot with millions of points, aggregating the data into a more coarse representation suitable for display.
+
+It can be used as a module of a larger application, because it doesn't block the UI, it computes the values between each animation frame.
+
+Inspired by the work of [Mike Bostock](https://github.com/d3/d3-plugins/tree/master/hexbin).
+
+
+#### Usage
+
+<a name="hist2d" href="#hist2d">#</a> <b>hist2d</b>([_data_], _callback_)
+
+Expects [_data_] to be an array of arrays: _[[1,2],[3,4],...]_  
+
+The _callback_ function is called when the binning is complete. It sends the data to its parameter.
+
+```js
+
+d3.hist2d()
+  .bins(40)
+  .domain([0, 100])
+  (data, draw);
+
+function draw(hist) {
+  // hist is the binned data
+}
+
+```
+
+The returned data is an array of bins. Each bin contains its position in columns and rows as x and y:
+
+* x - the column's position
+* y - the row's position
+
+Bins that are empty are omitted.
+
+<a name="bins" href="#bins">#</a> hexbin.__bins__(_bins_)
+
+Sets the number of columns and rows for the bins of the rectangular histogram.
+The total number of bins is `columns * rows === bins * bins`
+
+<a name="indices" href="#indices">#</a> hexbin.__indices__([_x index_, _y index_])
+
+Defines the indices of the data in the original array. This is useful if the data has more than two values or if the values are out of order.
+
+Example:
+```js
+[
+  [0, 43, 12248, 30],
+  [1, 45, 12398, 40],
+  [2, 46, 12456, 50],
+  ...
+]
+
+// we want to use [2] as x and [1] as y
+d3.hist2d()
+  .bins(40)
+  .indices([2, 1])
+  .domain([0, 100])
+  (data, draw);
+
+```
+
+If __indices__ is not set it defaults to `[0, 1]`.
+
+<a name="domain" href="#domain">#</a> hexbin.__domain__([ [_x domain_], [_y domain_] ])
+
+Sets the input domains for x and y. Expects an array with two arrays of numbers.
+
+<a name="size" href="#size">#</a> hexbin.__size__([_width, height_])
+
+`[width, height]` of the scatterplot. Sets the width and height of the cells `size / bins`.
+
+<a name="size_get" href="#size_get">#</a> hexbin.__size__()
+
+Returns an array with the width and height of the cells `[width, height]`.
+
+<a name="time" href="#time">#</a> hexbin.__time__(_time_)
+
+The binning function is non-blocking, so the values are computed between each animation frame for 12ms by default.
+
+#### Example
+
+...
+
+#### License & Acknowledgements
+
+LICENSE
---
+++ d3.hist2d.js
@@ -0,0 +1,119 @@
+(function() {
+
+  d3.hist2d = function() {
+    var b = 0; // bins
+    var d; // domain
+    var s; // size [width, height]
+    var xi = 0; // array index for x
+    var yi = 1; // array index for y
+    var x; // row scale
+    var y; // column scale
+    var t = 12; // time in ms to compute data between animation frames
+
+    function hist2d(data, callback) {
+      x = _quantize(d[0]);
+      y = _quantize(d[1]);
+
+      var hist = new Array(b * b);
+      i = hist.length;
+      while (i--) {
+        hist[i] = [];
+      }
+
+      var id = 0;
+      var hx = 0;
+      var hy = 0;
+      var now = 0;
+      var then = 0;
+      var i = data.length;
+
+      var iterate = function() {
+        if (i > 0) {
+          now = then = Date.now();
+          while (then - now < t && i-- > 0) {
+            hx = x(data[i][xi]);
+            hy = y(data[i][yi]);
+            id = hx * b + hy;
+
+            hist[id][hist[id].length] = data[i];
+
+            if (!hist[id].k) {
+              hist[id].x = hx;
+              hist[id].y = hy;
+              hist[id].k = true;
+            }
+            then = Date.now();
+          }
+          requestAnimationFrame(iterate);
+        } else {
+          hist = hist.filter(function(d) {
+            return d.k;
+          });
+          requestAnimationFrame(terminate);
+        }
+      };
+      requestAnimationFrame(iterate);
+
+      var terminate = function() {
+        callback(hist);
+      };
+
+      return hist2d;
+    }
+
+    // hist2d.bins(number) => b
+    hist2d.bins = function(_) {
+      if (!arguments.length) { return b; }
+      b = _;
+      return hist2d;
+    };
+
+    // hist2d.indices([x, y]) => [x, y]
+    hist2d.indices = function(_) {
+      if (!arguments.length) { return [xi, yi]; }
+      xi = _[0];
+      yi = _[1];
+      return hist2d;
+    };
+
+    // hist2d.time(number) => t
+    hist2d.time = function(_) {
+      if (!arguments.length) { return t; }
+      t = _;
+      return hist2d;
+    };
+
+    // hist2d.domain([[x],[y]]) => d
+    hist2d.domain = function(_) {
+      if (!arguments.length) { return d; }
+      d = _;
+      return hist2d;
+    };
+
+    // hist2d.size([width, height]) => s
+    hist2d.size = function(_) {
+      if (!arguments.length) { return s; }
+      s = [_[0] / b, _[1] / b];
+      return hist2d;
+    };
+
+    /**
+     * Quantize Credits:
+     * Mike Bostock (@mbostock)
+     * https://github.com/d3/d3-scale/blob/master/src/quantize.js
+     */
+
+    var _quantize = function(domain) {
+      var x0 = +domain[0];
+      var x1 = +domain[domain.length - 1];
+      var kx = b / (x1 - x0);
+      var i = b - 1;
+
+      return function(x) {
+        return Math.max(0, Math.min(i, Math.floor(kx * (x - x0))));
+      };
+    };
+
+    return hist2d;
+  };
+})();
